412b3d22eeb145bab97d60d805e706213e86f838
Calogero Crapanzano <calosan@libero.it>
2024-01-27 16:27:36 +0100

    Big fixing

--- pdf-merger/pdf-merger.js
+++ pdf-merger/pdf-merger.js
@@ -1,15 +1,17 @@
 const PDFMerger = require("pdf-merger-js");
-const merger = new PDFMerger();
+const fs = require('fs');
 
-const toMerge = async (payload, outFile) => {
-  payload.forEach(async (element) => {
-    await merger.add(element.fileName), element.page;
-  });
-  const mergedPdfBuffer = await merger.saveAsBuffer();
-  if (!outFile) return mergedPdfBuffer;
-  fs.writeSync("merged.pdf", mergedPdfBuffer);
-  return outFile;
-};
+async function merge(payload, outFile){
+  var merger = new PDFMerger();
+    for (const element of payload) {
+      await merger.add(element.fileName, element.page);
+    }
+    const mergedPdfBuffer = await merger.saveAsBuffer();
+    if (!outFile) return mergedPdfBuffer;
+    fs.writeFileSync(outFile, mergedPdfBuffer);
+    return outFile; 
+}
+
 module.exports = function (RED) {
   function pdfMerger(config) {
     RED.nodes.createNode(this, config);
@@ -18,7 +20,7 @@ module.exports = function (RED) {
       if (!Array.isArray(msg.payload)) node.error("Payload not array");
       const outFile = msg.outFile || config.outFile;
       try {
-        const merged = await toMerge(msg.payload, outFile);
+        const merged = await merge(msg.payload, outFile);
         msg.payload = merged;
         node.send(msg);
       } catch (error) {
